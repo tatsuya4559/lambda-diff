0bf84fdf90b11239f791a2f41b00cf9abd3d9845
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-07-27 13:49:12 +0900

    Use tempfile

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
-import shutil
 import subprocess
+import tempfile
 import zipfile
-from glob import glob
 
 import boto3
 import click
 import requests
 
 client = boto3.client('lambda')
+
 
 def download_file(url, filepath):
     '''
@@ -18,6 +18,7 @@ def download_file(url, filepath):
     with open(filepath, 'wb') as fp:
         fp.write(resp.content)
 
+
 def unzip_file(filepath):
     '''
     Args:
@@ -25,23 +26,20 @@ def unzip_file(filepath):
     Returns:
         path list of extracted files
     '''
-    dirname, _ = os.path.splitext(filepath)
-    if not os.path.exists(dirname):
-        os.mkdir(dirname)
-
+    dirname = os.path.dirname(filepath)
     with zipfile.ZipFile(filepath, 'r') as zip_ref:
         zip_ref.extractall(dirname)
     os.remove(filepath)
-    # return glob(os.path.join(dirname, '*'))
-    return dirname
 
-def get_src_code(func_name, version, base_or_head):
+
+def get_src_code(func_name, version, dirname):
     resp = client.get_function(FunctionName=func_name, Qualifier=version)
-    filepath = f'/tmp/{base_or_head}.zip'
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+    filepath = os.path.join(dirname, f'{func_name}.zip')
     download_file(resp['Code']['Location'], filepath)
-    src_dir = unzip_file(filepath)
-    print(src_dir)
-    return src_dir
+    unzip_file(filepath)
+
 
 @click.command()
 @click.option('--base', default='$LATEST', help='base version')
@@ -52,11 +50,12 @@ def diff(base, head, func_name):
     Example:
         $ python main.py lambdaFunctionName --base 2 --head 3
     '''
-    base_dir = get_src_code(func_name, base, 'base')
-    head_dir = get_src_code(func_name, head, 'head')
-    subprocess.run(['git', 'diff', base_dir, head_dir])
-    shutil.rmtree(base_dir)
-    shutil.rmtree(head_dir)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        base_dir = os.path.join(tmpdir, 'base')
+        head_dir = os.path.join(tmpdir, 'head')
+        get_src_code(func_name, base, base_dir)
+        get_src_code(func_name, head, head_dir)
+        subprocess.run(['git', 'diff', base_dir, head_dir])
 
 
 if __name__ == '__main__':
